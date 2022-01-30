14d4013b38c432c9f69d84ba2eaa6f9f5248b1fd
Devika Baid <devikabaid2015@gmail.com>
2022-01-30 09:17:56 +0530

    9:17AM

--- app.js
+++ app.js
@@ -26,55 +26,67 @@ const userSchema = new mongoose.Schema({
     admin:{type: Boolean}
 });
 const user = mongoose.model("User", userSchema);
+const defaultuser = new user({name:"None",email:"none",password:"none",employee:false, admin:false});
 
 const user1 = new user({name:"John",email:"abc@gmail.com",password:"hello",employee:false, admin:false});
 const user2 = new user({name:"Mary",email:"a@gmail.com",password:"hello",employee:false, admin:false});
 const emp1 = new user({name:"Rose",email:"adc@gmail.com",password:"hello",employee:true, admin:false, emplevel:2});
 const emp2 = new user({name:"Jasmin",email:"fsdc@gmail.com",password:"hello",employee:true, admin:false, emplevel:3});
 const admin1 = new user({name:"Peter",email:"fc@gmail.com",password:"hello",employee:true, admin:true, emplevel:5});
-user.insertMany([user1, user2, emp1, emp2, admin1], function(err){
-    if(err){console.log(err);}
-    else console.log("Successfully saved");
-});
+// user.insertMany([user1, user2, emp1, emp2, admin1], function(err){
+//     if(err){console.log(err);}
+//     else console.log("Successfully saved");
+// });
 
 const bugSchema = new mongoose.Schema({
     project: String,
     bugtitle: String,
     bugdesc: String,
     reported_by: userSchema,
-    assigned_to: [userSchema],
     date: Date,
-    threatlevel: Number
+    threatlevel: Number,
+    assigned: Boolean,
+    assigned_by: userSchema,
+    assigned_to: [userSchema],
+    assigned_on: Date,
+    deadline: Date,
+    resolved_on: Date,
+    resolved: Boolean,
+    report: String
 });
 const bug = mongoose.model("Bug", bugSchema);
 
-const bug1= new bug({project:"1",bugtitle:"bug1",bugdesc:"not opening",reported_by:user1,assigned_to:[emp1],date:new Date(),threatlevel:3});
-const bug2= new bug({project:"1",bugtitle:"bug2",bugdesc:"not closing",reported_by:user2,assigned_to:[emp2,emp2],date:new Date(),threatlevel:4});
-bug.insertMany([bug1, bug2], function(err){
-    if(err){console.log(err);}
-    else console.log("Successfully saved");
-});
+const bug1= new bug({project:"1",bugtitle:"bug1",bugdesc:"not opening",reported_by:user1,assigned:true,assigned_to:[emp1],date:new Date(),threatlevel:3});
+const bug2= new bug({project:"1",bugtitle:"bug2",bugdesc:"not closing",reported_by:user2,assigned:true,assigned_to:[emp2,emp2],date:new Date(),threatlevel:4});
+const bug3= new bug({project:"1",bugtitle:"bug3",bugdesc:"not closing",reported_by:user2,assigned:true,assigned_to:[emp2],date:new Date(),threatlevel:1});
+const bug4= new bug({project:"1",bugtitle:"bug4",bugdesc:"not closing",reported_by:user2,assigned:false,date:new Date(),threatlevel:1});
+// bug.insertMany([bug1, bug2, bug3, bug4], function(err){
+//     if(err){console.log(err);}
+//     else console.log("Successfully saved");
+// });
 
-const taskSchema = new mongoose.Schema({
-    bug: bugSchema,
-    assigned_by: userSchema,
-    assigned_to: userSchema,
-    assigned_on: Date,
-    resolved_on: Date,
-    resolved: Boolean
-});
-const task = mongoose.model("Task", taskSchema);
+// const taskSchema = new mongoose.Schema({
+//     bug: bugSchema,
+//     assigned_by: userSchema,
+//     assigned_to: userSchema,
+//     assigned_on: Date,
+//     deadline: Date,
+//     resolved_on: Date,
+//     resolved: Boolean
+// });
+// const task = mongoose.model("Task", taskSchema);
 
-const task1 = new task({bug:bug1,assigned_by:admin1,assigned_to:emp1,assigned_on:new Date(),resolved:false})
-const task2 = new task({bug:bug2,assigned_by:admin1,assigned_to:emp2,assigned_on:new Date(),resolved:false})
-task.insertMany([task1,task2], function(err){
-    if(err){console.log(err);}
-    else console.log("Successfully saved");
-});
+// const task1 = new task({bug:bug1,assigned_by:admin1,assigned_to:emp1,assigned_on:new Date(),resolved:false})
+// const task2 = new task({bug:bug2,assigned_by:admin1,assigned_to:emp2,assigned_on:new Date(),resolved:false})
+// task.insertMany([task1,task2], function(err){
+//     if(err){console.log(err);}
+//     else console.log("Successfully saved");
+// });
 
 const chatSchema = new mongoose.Schema({
     user_:userSchema,
     emp_:userSchema,
+    bug:bugSchema,
     messages: [{ message_body: String, user_message: Boolean }],
 });
 const chat = mongoose.model("Chat",chatSchema);
@@ -85,73 +97,256 @@ app.use(express.static("public"));
 //   "mongodb+srv://user1:user1@cluster0.mcznv.mongodb.net/todolistDB"
 // );
 
-let currentuser=new user();
+let currentuser=defaultuser;
 let alertSignIn="";
 let alertSignUp="";
-app.post("/login",function(req,res){
-    console.log(req.body);
-    
-    const user_email=req.body.email;
-    user.find({email:user_email},function(err,found){
+// app.post("/signup")
+app.post("/signin",function(req,res){
+    // console.log(req.body);
+    user.find({email:req.body.your_email},function(err,found){
+        if(!err){
+            if(found.length===0){
+                alertSignIn="Email is not registered, sign up first."
+                res.redirect("/signin");
+            }
+            else{
+                if(found[0].password===req.body.your_pass){
+                    if(found[0].employee){
+                        currentuser=found[0];
+                        res.render("homeemp");}
+                    else{
+                        currentuser=found[0];
+                        res.render("homeuser");
+                    }
+                }
+                else{
+                    alertSignIn="Password incorrect.";
+                    res.redirect("/signin");
+                }
+            }
+        }else{console.log(err);}
+    });
+});
+
+app.post("/signup",function(req,res){
+    user.find({email:req.body.email},function(err,found){
         console.log(found);
-        if(found.length===0){  
-            if(req.body.signup==="Register"){
-                const newuser= new user({name:req.body.your_name, email:req.body.your_email, password:req.body.your_pass,employee:false, admin:false});
+        if(!err){
+            if(found.length===0){
+                const newuser= new user({name:req.body.name, email:req.body.email, password:req.body.pass,employee:false, admin:false});
                     user.insertMany([newuser],function(err){
                         if (err) {
                             console.log(err);}
                     });
                 currentuser= newuser;
-                res.redirect("homeuser");
-                }
-                else{
-                    alertSignIn="You don't have an account with this email,try Sign up";
-                    res.redirect("/login");
-                }
-        }
-        else{
-            if(req.body.signin==="Sign in"){  
-                currentuser= found[0];
-                if(found.employee===true){res.redirect("homeemp");}
-                else{res.redirect("homeuser");}
+                res.render("homeuser");
             }
             else{
-                alertSignUp="You already have an account with this email,try Sign in";
-                res.redirect("/login");
+                alertSignUp="Account already exist with this email, try signing in.";
+                res.redirect("/signup");
             }
-        }
-    })
-
-    // res.redirect("homeuser");
+        }else{console.log(err);}
+    });
 });
+
 app.post("/bug",function(req,res){
-    console.log(currentuser);
-    const newbug= new bug({project:req.body.state,bugtitle:req.body.Title ,bugdesc:req.body.desc,date:new Date(),threatlevel:req.body.threatlevel})
+    if(currentuser===defaultuser){res.redirect("/signin")};
+    // console.log("*");
+    // console.log(currentuser);
+    const newbug= new bug({
+        project:req.body.state,
+        bugtitle:req.body.Title ,
+        bugdesc:req.body.desc,
+        date:new Date(),
+        threatlevel:req.body.threatlevel,
+        reported_by:currentuser,
+        assigned:true,
+        assigned_to:[emp1],
+        resolved:false
+    });
+    console.log(newbug);
     bug.insertMany([newbug],function(err){
         if (err) {
             console.log(err);}
     });
+    // const newtask = new task({bug:newbug,resolved:false})
+    // task.insertMany([newtask],function(err){
+    //     if (err) {
+    //         console.log(err);}
+    // });
+    if(currentuser.employee)res.redirect("/homeemp");
+    else res.redirect("/homeuser");
+});
+app.get("/resolved/:bugid",function(req,res){
+    // if(currentuser===defaultuser){res.redirect("/signin")};
+    // const onetask=bug.findById(req.params.bugid);
+        // console.log("\n\n\nreached\n\n\n");
+        bug.updateOne({_id:req.params.bugid},{resolved:true},function(err){console.log(err);});
+        bug.findOne({ _id:req.params.bugid }, function (err, found) {
+            if(!err){
+                // console.log(found);
+                console.log("\n\n"+found.resolved+"\n\n");
+                // res.render("taskemp",{task:found});
+                res.send("report page with post <a>submit</a>");
+
+            }
+            else{console.log(err);}
+        });
 });
 
 app.get("/", function (req, res) {
-    res.render("login");
+    res.render("signin");
 });
-
+app.get("/report/:bugid",function(req,res){
+    res.render("report",{bugid:req.params.bugid});
+});
+app.post("/report/:bugid",function(req,res){
+    console.log(req.body);
+    console.log(req.params);
+    bug.updateOne({_id:req.params.bugid},{report:req.body.bugREPORT,resolved:true},function(err){console.log(err);});
+    res.redirect("/taskemp/"+req.params.bugid);
+});
 app.get("/homeuser",function (req, res) {
+    console.log(currentuser);
+    if(currentuser===defaultuser){res.redirect("/signin")};
     res.render("homeuser");
 });
 
 app.get("/homeemp",function (req, res) {
+    if(currentuser===defaultuser){res.redirect("/signin")};
     res.render("homeemp");
 });
 
-app.get("/login", function (req, res) {
-  res.render("login",{alertSignUp:alertSignUp,alertSignIn:alertSignIn});
+app.get("/signin", function (req, res) {
+  res.render("signin",{alertSignIn:alertSignIn});
 });
+app.get("/signup", function (req, res) {
+    res.render("signup",{alertSignUp:alertSignUp});
+  });
 
 app.get("/bug", function (req, res) {
+    if(currentuser===defaultuser){res.redirect("/signin")};
     console.log(currentuser);
     res.render("bug",{alertSignIn:alertSignIn,alertSignUp:alertSignUp});
+});
+
+app.get("/alltask",function(req,res){
+    if(currentuser===defaultuser){res.redirect("/signin")};
+    bug.find({threatlevel:{$lt:currentuser.emplevel+1}},function(err,found){//threatlevel:{$lt:currentuser.emplevel-1}
+        res.render("alltask",{allTasks:found});
+    });
+});
+
+app.get("/mytasks",function(req,res){
+    if(currentuser===defaultuser){res.redirect("/signin")};
+    bug.find({assigned:true},function(err,foundassigned){
+        const myTasks=[];
+        for(var i=0;i<foundassigned.length;i++){
+            for(var j=0;j<foundassigned[i].assigned_to.length;j++){
+                if(foundassigned[i].assigned_to[j].email===currentuser.email){
+                    myTasks.push(foundassigned[i]);
+                }
+            }
+        }
+        res.render("mytasks",{myTasks:myTasks});
+    });
+});
+
+app.get("/myTeam",function (req, res) {
+    if(currentuser===defaultuser){res.redirect("/signin")};
+    res.render("myTeam");
+});
+
+app.get("/taskemp/:bugid",function (req, res) {
+    if(currentuser===defaultuser){res.redirect("/signin")};
+    // const onetask=bug.findById(req.params.bugid);
+  bug.findOne({ _id:req.params.bugid }, function (err, found) {
+    if(!err){
+        console.log(found);
+        res.render("taskemp",{task:found});
+    }else{console.log(err);}
+  });
+    
+});
+
+app.get("/request",function (req, res) {
+    // if(currentuser===defaultuser){res.redirect("/signin")};
+    res.render("request");
+});
+
+app.get("/adminteam",function(req,res){
+    res.render("adminmyteam");
+});
+app.get("/adminteam/:userid",function(req,res){
+    console.log(req.params.userid);
+    res.render("adminmyteam");
+    // if(currentuser===defaultuser){res.redirect("/signin")};
+    // const onetask=bug.findById(req.params.bugid);
+        // console.log("\n\n\nreached\n\n\n");
+        // bug.updateOne({_id:req.params.bugid},{resolved:true},function(err){console.log(err);});
+        // bug.findOne({ _id:req.params.bugid }, function (err, found) {
+        //     if(!err){
+        //         // console.log(found);
+        //         console.log("\n\n"+found.resolved+"\n\n");
+        //         // res.render("taskemp",{task:found});
+        //         res.render("adminmyteam");
+        //     }
+        //     else{console.log(err);}
+        // });
+});
+
+app.get("/addemp",function(req,res){
+    res.render("addemp");
+});
+app.post("/addemp",function(req,res){
+    console.log(req.body);
+    user.find({email:req.body.email},function(err,found){
+        var adminlevel=req.body.level===5;
+        console.log(found);
+        if(!err){
+            if(found.length!==0){
+                user.deleteOne({email:req.body.name},function(err){
+                    if(err){console.log(err);}
+                    else{console.log("Successfully deleted");}
+                });    
+            }
+            // if(req.body.level===5){}
+            const newuser= new user({name:req.body.name, email:req.body.email, password:req.body.pass,employee:true,emplevel:req.body.level, admin:adminlevel});
+            console.log(newuser);
+            user.insertMany([newuser],function(err){
+                    if (err) {console.log(err);}
+                    else{console.log("successfully added");}
+            });
+            // currentuser= newuser;
+            res.render("adminmyteam");
+        }else{console.log(err);}
+    });
+});
+
+app.get("/raisedbug",function(req,res){
+    if(currentuser===defaultuser){res.redirect("/signin")};
+    bug.find({},function(err,found){
+        var raisedbugs=[];
+        console.log(found.length);
+        console.log("*"+currentuser.name+"*");
+        found.forEach(function(bug){
+            console.log("*"+bug.reported_by.name+"*");
+            if(bug.reported_by.name===currentuser.name){
+                console.log("inside iffff");
+                raisedbugs.push(bug);
+            }
+        });
+        console.log(raisedbugs.length);
+        res.render("raisedbug",{raisedbugs:raisedbugs});
+    });
+});
+
+app.get("/resolvedbugs",function(req,res){
+    if(currentuser===defaultuser){res.redirect("/signin")};
+    bug.find({resolved:true},function(err,found){
+        if(err) console.log(err);
+        else res.render("resolvedbugs",{resolvedbugs:found});
+    });
 });
 
 app.listen(3000, function () {
